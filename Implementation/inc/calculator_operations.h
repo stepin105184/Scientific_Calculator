4f603e7ca5f5a3f7f0bd1ffec1af731ef67d6086
99002655 <71481410+99002655@users.noreply.github.com>
2020-09-19 00:23:27 +0530

    Update calculator_operations.h

--- Implementation/inc/calculator_operations.h
+++ Implementation/inc/calculator_operations.h
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdio_ext.h>
+#include <math.h>
 
 /**
 *  adds the operand1 and operand2 and returns the result
@@ -43,5 +44,19 @@ int multiply(int operand1, int operand2);
 * @note returns 0 for divide by 0 error
 */
 int divide(int operand1, int operand2);
+float power(int operand1, int operand2);
+float logten(int operand1);
+float exponent(int operand1);
+float square_root(int operand1);
+float cube_root(int operand1);
+float sine(float operand3);
+float cosine(float operand3);
+float tangent(float operand3);
+float sineh(float operand3);
+float cosineh(float operand3);
+float tangenth(float operand3);
+float sineinv(float operand3);
+float cosineinv(float operand3);
+float tangentinv(float operand3);
 
 #endif  /* #define __CALCULATOR_OPERATIONS_H__ */
