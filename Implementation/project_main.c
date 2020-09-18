c4f787373ecf531fcab011f7041f39e8538fff6c
99002655 <71481410+99002655@users.noreply.github.com>
2020-09-19 00:21:10 +0530

    Add files via upload

---
+++ Implementation/project_main.c
@@ -0,0 +1,125 @@
+#include <calculator_operations.h>
+
+/* Status of the operation requested */
+#define VALID   (1)
+#define INVALID (0)
+
+/* Calculator operation requested by user*/
+unsigned int calculator_operation = 0;
+
+/* Operands on which calculation is performed */
+int calculator_operand1 = 0;
+int calculator_operand2 = 0;
+float calculator_operand3 = 0;
+
+/* Valid operations */
+enum operations{ ADD=1, SUBTRACT, MULTIPLY, DIVIDE,POWER,LOG,EXP,SQUAREROOT,CUBE,SINE,COSE,TAN,SINEH,COSEH,TANH,ISINE,ICOSE,ITAN,EXIT };
+/* Display the menu of operations supported */
+void calculator_menu(void);
+/* Verifies the requested operations validity */
+int valid_operation(int operation);
+
+
+/* Start of the application */
+int main(int argc, char *argv[])
+{
+    printf("\n****Calculator****\n");
+    while(1)
+    {
+        calculator_menu();
+    }
+}
+
+void calculator_menu(void)
+{
+    printf("\nAvailable Operations\n");
+    printf("\n1. Add\n2. Subtract\n3. Multiply\n4. Divide\n5.Power\n6.LOG\n7.Exponent\n8.Square\n9.Cube\n10.Sine\n11.COSE\n12.TAN\n13.SINEH\n14.COSEH\n15.TANH\n16.ISINE\n17.ICOSE\n18.ITAN\n19. Exit");
+    printf("\n\tEnter your choice\n");
+   
+     __fpurge(stdin);
+    scanf("%d", &calculator_operation);
+
+    if(EXIT == calculator_operation)
+    {
+        printf("\nThank you. Exiting the Application\n");
+        exit(0);
+    }
+
+    if(INVALID != valid_operation(calculator_operation))
+    {
+        printf("\n\tEnter your Numbers with space between them\n");
+        __fpurge(stdin);
+        scanf("%d %d", &calculator_operand1, &calculator_operand2);
+    }
+    else
+    {
+        printf("\n\t---Wrong choice---\nEnter to continue\n");
+        __fpurge(stdin);
+        getchar();
+        return;
+        
+    }
+    switch(calculator_operation)
+    {
+        case ADD:
+            printf("\n\t%d + %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            add(calculator_operand1, calculator_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+        case SUBTRACT:
+            printf("\n\t%d - %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            subtract(calculator_operand1, calculator_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+        case MULTIPLY:
+            printf("\n\t%d * %d = %d\nEnter to continue", 
+            calculator_operand1, 
+            calculator_operand2,
+            multiply(calculator_operand1, calculator_operand2));
+            
+            __fpurge(stdin);
+            getchar();
+            break;
+        case DIVIDE:
+            printf(divide(calculator_operand1, calculator_operand2));
+            
+            
+            break;
+	case POWER:printf(power(calculator_operand1, calculator_operand2)); break;
+	case POWER:printf("%f",power(calculator_operand1, calculator_operand2)); break;
+	case LOG:printf("%f",logten(calculator_operand1));break;
+	case EXP:printf("%f",exponent(calculator_operand1));break;
+	case SQUAREROOT:printf("%f",square_root(calculator_operand1));break;
+	case CUBE:printf("%f",cube_root(calculator_operand1));break;
+	case SINE:printf("%f",sine(calculator_operand3));break;
+	case COS:printf("%f",cosine(calculator_operand3));break;
+	case TAN:printf("%f",tangent(calculator_operand3));break;
+	case SINEH:printf("%f",sineh(calculator_operand3));break;
+	case COSEH:printf("%f",cosineh(calculator_operand3));break;
+	case TANH:printf("%f",tangenth(calculator_operand3));break;
+	case ISINE:printf("%f",sineinv(calculator_operand3));break;
+	case ICOSE:printf("%f",cosineinv(calculator_operand3));break;
+	case ITAN:printf("%f",tangentinv(calculator_operand3));break;
+
+
+        case 19:
+            exit(0);
+            break;
+        default:
+            printf("\n\t---It should never come here---\n");
+    }
+}
+
+int valid_operation(int operation)
+{
+    /* Check if the operation is a valid operation */
+    return ((ADD <= operation) && (EXIT >= operation)) ? VALID: INVALID;
+}
---
+++ Implementation/src/calculator_operations.c
@@ -0,0 +1,92 @@
+#include <calculator_operations.h>
+
+int add(int operand1, int operand2)
+{
+    return operand1 + operand2;
+}
+
+int subtract(int operand1, int operand2)
+{
+    return operand1 - operand2;
+}
+
+int multiply(int operand1, int operand2)
+{
+    return operand1 * operand2;
+}
+
+int divide(int operand1, int operand2)
+{
+    if(0 == operand2)
+        return 0;
+    else
+        return operand1 / operand2;
+}
+
+float square_root(int operand1)
+{
+    return sqrt(operand1);
+}
+
+float cube_root(int operand1)
+{
+    return cbrt(operand1);
+}
+
+float logten(int operand1)
+{
+    return (log10(operand1));
+}
+
+float exponent(int operand1)
+{
+    return (exp(operand1));
+}
+
+float power(int operand1,int operand2)
+{
+    return pow(operand1,operand2);
+}
+
+float sine(float operand3)
+{
+    float x;
+    x = operand3 * (3.142 / 180.0);
+    return (sin(x));
+}
+float cosine(float operand3)
+{
+    float x;
+    x = operand3 * (3.142 / 180.0);
+    return (cos(x));
+}
+float tangent(float operand3)
+{
+    float x;
+    x = operand3 * (3.142 / 180.0);
+    return (tan(x));
+}
+float sineh(float operand3)
+{
+    return (sinh(operand3));
+}
+float cosineh(float operand3)
+{
+    return (cosh(operand3));
+}
+float tangenth(float operand3)
+{
+    return (tanh(operand3));
+}
+float sineinv(float operand3)
+{
+    return (asin(operand3));
+}
+float cosineinv(float operand3)
+{
+    return (acos(operand3));
+}
+float tangentinv(float operand3)
+{
+    return (atan(operand3));
+}
