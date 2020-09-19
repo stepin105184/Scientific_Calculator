cec44f52b3c3a993d1ba91d43c7fa69932d307ba
99002655 <71481410+99002655@users.noreply.github.com>
2020-09-19 10:52:40 +0530

    Update test_calculator_operations.c

--- Implementation/test/test_calculator_operations.c
+++ Implementation/test/test_calculator_operations.c
@@ -11,6 +11,19 @@ void test_subtract(void);
 void test_multiply(void);
 void test_divide(void);
 void test_power(void);
+void test_log(void);
+void test_exponent(void);
+void test_squreroot(void);
+void test_cuberoot(void);
+void test_sine(void);
+void test_cosine(void);
+void test_tangent(void);
+void test_sineh(void);
+void test_cosineh(void);
+void test_tangenth(void);
+void test_sineinv(void);
+void test_cosineinv(void);
+void test_tangentinv(void);
 
 /* Start of the application test */
 int main() {
@@ -28,6 +41,19 @@ int main() {
   CU_add_test(suite, "multiply", test_multiply);
   CU_add_test(suite, "divide", test_divide);
   CU_add_test(suite, "power", test_power);
+  CU_add_test(suite, "log", test_log);
+  CU_add_test(suite, "exp", test_exponent);
+  CU_add_test(suite, "square root", test_squareroot);
+  CU_add_test(suite, "cube root", test_cuberoot);
+  CU_add_test(suite, "sine", test_sine);
+  CU_add_test(suite, "cosine", test_cosine);
+  CU_add_test(suite, "tangent", test_tangent);
+  CU_add_test(suite, "sinh", test_sineh);
+  CU_add_test(suite, "cosh", test_cosineh);
+  CU_add_test(suite, "tanh", test_tangenth);
+  CU_add_test(suite, "sine inv", test_sineinv);
+  CU_add_test(suite, "cosine inv", test_cosineinv);
+  CU_add_test(suite, "tangent inv", test_tangentinv);
 
 
 /* Note: Do not edit START*/
@@ -68,5 +94,57 @@ void test_divide(void) {
 }
 void test_power(void) {
   CU_ASSERT(100 == power(10,2));
+  
+}
+void test_log(void) {
+  CU_ASSERT(2== logten(100));
+  
+}
+void test_exponent(void) {
+  CU_ASSERT(1.64== exponent(.5));
+  
+}
+void test_squareroot(void) {
+  CU_ASSERT(2== square_root(4));
+  
+}
+void test_cuberoot(void) {
+  CU_ASSERT(2== cube_root(8));
+  
+}
+void test_sine(void) {
+  CU_ASSERT(1== sine(90));
+  
+}
+void test_cosine(void) {
+  CU_ASSERT(0== cosine(90));
+  
+}
+void test_tangent(void) {
+  CU_ASSERT(1== tangent(45));
+  
+}
+void test_sineh(void) {
+  CU_ASSERT(3.62== sineh(2));
+  
+}
+void test_cosineh(void) {
+  CU_ASSERT(3.76== cosineh(2));
+  
+}
+void test_tangenth(void) {
+  CU_ASSERT(0.9== tangenth(2));
+  
+}
+void test_sineinv(void) {
+  CU_ASSERT(0.52== sineinv(0.5));
+  
+}
+void test_cosineinv(void) {
+  CU_ASSERT(1.04== cosineinv(0.5));
+  
+}
+void test_tangentinv(void) {
+  CU_ASSERT(0.46== tangentinv(0.5));
   
 }
